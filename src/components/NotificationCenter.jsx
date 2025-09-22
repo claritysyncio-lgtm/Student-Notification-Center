70f48cf7d8ec29601724ca2cb13df6504c591a01
claritysyncio-lgtm <claritysync.io@gmail.com>
2025-09-21 21:32:46 -0400

    Add black and white Notion-style callout around filters with Notification Center title

--- src/components/NotificationCenter.jsx
+++ src/components/NotificationCenter.jsx
@@ -71,20 +71,23 @@ export default function NotificationCenter() {
 
   return (
     <div className="nc-root">
-      <header className="nc-header">
-        <div className="nc-filters">
-          <Dropdown label="All Courses" options={["All Courses", ...courses]} value={courseFilter} onChange={setCourseFilter} />
-          <Dropdown label="All Types" options={["All Types", ...types]} value={typeFilter} onChange={setTypeFilter} />
-        </div>
-        <button className="refresh-button" onClick={() => window.location.reload()} title="Refresh tasks">
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/>
-            <path d="M21 3v5h-5"/>
-            <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/>
-            <path d="M3 21v-5h5"/>
-          </svg>
-        </button>
-      </header>
+      <div className="nc-callout">
+        <div className="nc-callout-title">Notification Center</div>
+        <header className="nc-header">
+          <div className="nc-filters">
+            <Dropdown label="All Courses" options={["All Courses", ...courses]} value={courseFilter} onChange={setCourseFilter} />
+            <Dropdown label="All Types" options={["All Types", ...types]} value={typeFilter} onChange={setTypeFilter} />
+          </div>
+          <button className="refresh-button" onClick={() => window.location.reload()} title="Refresh tasks">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/>
+              <path d="M21 3v5h-5"/>
+              <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/>
+              <path d="M3 21v-5h5"/>
+            </svg>
+          </button>
+        </header>
+      </div>
 
       {loading && (
         <div className="nc-loading">Loading tasks from Notion...</div>
