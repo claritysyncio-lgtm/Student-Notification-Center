ace78892d236407ee5b6caff0a534631924a2635
claritysyncio-lgtm <claritysync.io@gmail.com>
2025-09-23 16:58:33 -0400

    Fix Vercel deployment: Update project name to avoid conflict
    - Changed project name from 'student-notification-center' to 'student-notification-center-v2'
    - Updated OAuth configuration to use new deployment URL
    - Resolves Vercel deployment conflict error

--- src/config/oauthConfig.js
+++ src/config/oauthConfig.js
@@ -9,7 +9,7 @@
 const NOTION_CLIENT_ID = '277d872b-594c-8009-a497-003712a5baf8';
 
 // Base URL for the application - update this when deploying
-const BASE_URL = 'https://student-notification-center.vercel.app';
+const BASE_URL = 'https://student-notification-center-v2.vercel.app';
 
 export const OAUTH_CONFIG = {
   /**
